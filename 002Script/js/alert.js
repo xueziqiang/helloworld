f389d9993b2669476de16cace732d895259947b7
johnny <qmissr@qq.com>
2020-09-12 21:13:10 +0800

    modify

---
+++ 002Script/js/alert.js
@@ -0,0 +1,12 @@
+function sayhi()
+{
+    alert("你在做什么");
+    alert("点一下就好");
+    alert("再点一下呢");
+    alert("哈哈哈，什么都没有");
+}
+alert("1-你在做什么");
+sayhi();
+alert("点一下就好");
+alert("再点一下呢");
+alert("哈哈哈，什么都没有");
